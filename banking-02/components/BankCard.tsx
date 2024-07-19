ab50b9f4f7150bf1335b9d17d904570e6e014452
Obimbo07 <obimboausts@gmail.com>
2024-07-19 16:32:32 +0300

    Feature: implemented bank card redesign

--- banking-02/components/BankCard.tsx
+++ banking-02/components/BankCard.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from '@/lib/utils'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -15,7 +16,41 @@ const BankCard = ({ account, userName, showBalance = true}: CreditCardProps) => 
                 {formatAmount(account.currentBalance)}
               </p>
             </div>
+            <article className="flex flex-col gap-2">
+                <div className='flex justify-between'>
+                  <h1 className='text-12 font-semibold text-white'>
+                    {userName}
+                  </h1>
+                  <h1 className='text-12 font-semibold text-white'>
+                    ** /** 
+                  </h1>
+                </div>
+                <p className='text-14 font-semibold tracking-[1.1px] text-white'>
+                ●	●	● 	●	●	●	● 	●	●	●	<span className='text-16'>12345</span>
+                </p>
+            </article>
          </div>
+         <div className='bank-card_icon'>
+            <Image 
+              src='/icons/Paypass.svg'
+              width={20}
+              height={20}
+              alt='pay'
+              />
+              <Image 
+              src='/icons/mastercard.svg'
+              width={20}
+              height={20}
+              alt='mastercard'
+              />
+         </div>
+         <Image 
+              src='/icons/lines.svg'
+              width={216}
+              height={190}
+              alt='lines'
+              className='absolute left-0 top-0'
+              />
        </Link>
     </div>
   )
